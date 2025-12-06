33e80a0536385a52a4003da2e835e3125ca99575
Kameron Dawson <ksdawson@mit.edu>
2025-12-06 17:24:17 -0500

    Implement VL2 topology

---
+++ network/vl2.py
@@ -0,0 +1,68 @@
+from mininet.topo import Topo
+from mininet.link import TCLink
+from utils import visualize_topo
+
+# Uses a clos like topology
+# Hosts connect to ToR switches
+# ToR switches connect to aggregate switches
+# Aggregate switches connect to intermediate switches
+
+class VL2Topo(Topo):
+    def __init__(self, D_A=8, D_I=8, server_link=1, switch_link=10):
+        """
+        Setups a VL2 datacenter network according to https://dl.acm.org/doi/10.1145/1594977.1592576.
+        Args:
+            D_A (int): Number of ports on aggregation switches.
+            D_I (int): Number of ports on intermediate switches.
+            server_link (int): Server link bandwidth in Gbps.
+            switch_link (int): Switch link bandwidth in Gbps.
+        """
+        # Setup base class
+        super(VL2Topo, self).__init__()
+
+        # Num of nodes
+        num_inter = D_A / 2
+        num_aggr = D_I
+        num_tor = D_A * D_I / 4
+        num_host = 20 * num_tor
+
+        # Setup nodes
+        self._inter_switches = {f'i{i}':self.addSwitch(f'i{i}') for i in range(num_inter)}
+        self._aggr_switches = {f'a{a}':self.addSwitch(f'a{a}') for a in range(num_aggr)}
+        self._tor_switches = {f't{t}':self.addSwitch(f't{t}') for t in range(num_tor)}
+        self._hosts = {f'h{h}':self.addHost(f'h{h}') for h in range(num_host)}
+
+        # Connect hosts to ToRs
+        for t in range(num_tor):
+            tor = self._tor_switches[f't{t}']
+            for h in range(20):
+                host = self._hosts[f'h{t*20+h}']
+                self.addLink(host, tor, cls=TCLink,
+                    bw=server_link, # Mbps
+                    delay='0ms',
+                    loss=1, # %
+                    max_queue_size=100 # num packets
+                )
+        # Connect ToRs to aggregate switches
+        for a in range(num_aggr):
+            aggr = self._aggr_switches[f'a{a}']
+            for t in range(D_A/2):
+                tor = self._tor_switches[f't{a*(D_A/2)+t}']
+                self.addLink(tor, aggr, cls=TCLink,
+                    bw=switch_link, delay='0ms', loss=1, max_queue_size=100
+                )
+        # Connect aggregate to intermediate switches
+        for i in range(num_inter):
+            inter = self._inter_switches[f'i{i}']
+            for a in range(D_I):
+                aggr = self._aggr_switches[f'a{i*D_I+a}']
+                self.addLink(aggr, inter, cls=TCLink,
+                    bw=switch_link, delay='0ms', loss=1, max_queue_size=100
+                )
+
+if __name__ == '__main__':
+    # Make clos network
+    topo = VL2Topo(D_A=2, DI_2=2)
+
+    # Visualize topo
+    visualize_topo(topo, 'vl2_topology.png')
