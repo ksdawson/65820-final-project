d0efa0d776d910b9753800daaa96352929a81dd9
Kameron Dawson <ksdawson@mit.edu>
2025-12-07 19:07:16 -0500

    ok lets start simple

--- network/vl2_switch.py
+++ network/vl2_switch.py
@@ -1,40 +1,67 @@
-import networkx as nx
-from ryu.topology import event
-from ryu.topology.api import get_switch, get_link
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
+from ryu.lib.packet import packet, ethernet, ether_types
 from ryu.app.simple_switch_13 import SimpleSwitch13
 import logging
 
-class VL2Switch(SimpleSwitch13):
+class VL2Controller(SimpleSwitch13):
     def __init__(self, *args, **kwargs):
-        super(VL2Switch, self).__init__(*args, **kwargs)
+        # Setup
+        super(VL2Controller, self).__init__(*args, **kwargs)
         self.logger.setLevel(logging.WARNING) # don't print everything
-        self.net_graph = nx.DiGraph()
 
-    @set_ev_cls(event.EventSwitchEnter)
-    def handler_switch_enter(self, ev):
-        self._update_topology()
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
-    @set_ev_cls(event.EventLinkAdd)
-    def handler_link_add(self, ev):
-        self._update_topology()
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-    def _update_topology(self):
-        # Clear current graph to rebuild (simplest approach)
-        self.net_graph.clear()
-        
-        # Get all switches
-        switch_list = get_switch(self, None)
-        switches = [switch.dp.id for switch in switch_list]
-        self.net_graph.add_nodes_from(switches)
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
 
-        # Get all links
-        link_list = get_link(self, None)
-        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) 
-                 for link in link_list]
-        self.net_graph.add_edges_from(links)
+        dpid = format(datapath.id, "d").zfill(16)
+        self.mac_to_port.setdefault(dpid, {})
 
-        # Print the graph
-        self.logger.info("--- Current Topology ---")
-        self.logger.info("Nodes: %s", self.net_graph.nodes())
-        self.logger.info("Edges: %s", self.net_graph.edges())
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
